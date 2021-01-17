76f8e564859cfd05ca1aa4cc14c358c4d283fe93
Mihail Isakov <mihail.isakov@gmail.com>
2021-01-17 04:20:30 +0100

    MDCM fixed typo

--- mdcm/Source/MediaStorageAndFileFormat/mdcmJPEGCodec.cxx
+++ mdcm/Source/MediaStorageAndFileFormat/mdcmJPEGCodec.cxx
@@ -34,7 +34,7 @@
 namespace mdcm
 {
 
-JPEGCodec::JPEGCodec():BitSample(0),Quality(100)
+JPEGCodec::JPEGCodec() : BitSample(0), Quality(100)
 {
   Internal = NULL;
 }
@@ -75,7 +75,7 @@ bool JPEGCodec::CanDecode(TransferSyntax const &ts) const
       || ts == TransferSyntax::JPEGLosslessProcess14_1;
 }
 
-bool JPEGCodec::CanCode(TransferSyntax const &ts) const
+bool JPEGCodec::CanCode(TransferSyntax const & ts) const
 {
   return ts == TransferSyntax::JPEGBaselineProcess1
       || ts == TransferSyntax::JPEGExtendedProcess2_4
@@ -86,10 +86,14 @@ bool JPEGCodec::CanCode(TransferSyntax const &ts) const
       || ts == TransferSyntax::JPEGLosslessProcess14_1;
 }
 
-void JPEGCodec::SetPixelFormat(PixelFormat const &pt)
+void JPEGCodec::SetPixelFormat(PixelFormat const & pf)
 {
-  ImageCodec::SetPixelFormat(pt);
-  SetBitSample(pt.GetBitsAllocated());
+  ImageCodec::SetPixelFormat(pf);
+#if 0
+  SetBitSample(pf.GetBitsAllocated());
+#else
+  SetBitSample(pf.GetBitsStored());
+#endif
 }
 
 void JPEGCodec::SetupJPEGBitCodec(int bit)
@@ -97,7 +101,6 @@ void JPEGCodec::SetupJPEGBitCodec(int bit)
   BitSample = bit;
   delete Internal;
   Internal = NULL;
-  // single bit images?
   if (BitSample <= 8)
   {
     mdcmDebugMacro("Using JPEG8");
@@ -115,7 +118,6 @@ void JPEGCodec::SetupJPEGBitCodec(int bit)
   }
   else
   {
-    // mdcmNonImageData/RT/RTDOSE.dcm
     mdcmAlwaysWarnMacro("JPEG: cannot instantiate codec for bit sample " << bit);
     delete Internal;
     Internal = NULL;
@@ -141,8 +143,8 @@ bool JPEGCodec::Decode(DataElement const &in, DataElement &out)
 {
   assert(Internal);
   out = in;
-  const SequenceOfFragments *sf0 = in.GetSequenceOfFragments();
-  const ByteValue *jpegbv = in.GetByteValue();
+  const SequenceOfFragments * sf0 = in.GetSequenceOfFragments();
+  const ByteValue * jpegbv = in.GetByteValue();
   if(!sf0 && !jpegbv)
   {
     mdcmAlwaysWarnMacro("JPEG: !sf0 && !jpegbv");
@@ -154,20 +156,24 @@ bool JPEGCodec::Decode(DataElement const &in, DataElement &out)
     for(unsigned int i = 0; i < sf0->GetNumberOfFragments(); ++i)
     {
       std::stringstream is;
-      const Fragment &frag = sf0->GetFragment(i);
+      const Fragment & frag = sf0->GetFragment(i);
       if(frag.IsEmpty())
       {
         mdcmAlwaysWarnMacro("JPEG: frag.IsEmpty()");
         return false;
       }
-      const ByteValue &bv = dynamic_cast<const ByteValue&>(frag.GetValue());
-      size_t bv_len = bv.GetLength();
-      char *mybuffer = new char[bv_len];
-      bool b = bv.GetBuffer(mybuffer, bv.GetLength());
-      assert(b); (void)b;
-      is.write(mybuffer, bv.GetLength());
-      delete[] mybuffer;
-      bool r = DecodeByStreams(is, os);
+      const ByteValue & bv = dynamic_cast<const ByteValue&>(frag.GetValue());
+      const size_t bv_len = bv.GetLength();
+      char * mybuffer = new char[bv_len];
+      const bool b = bv.GetBuffer(mybuffer, bv_len);
+      if (!b)
+      {
+        delete [] mybuffer;
+        return false;
+      }
+      is.write(mybuffer, bv_len);
+      delete [] mybuffer;
+      const bool r = DecodeByStreams(is, os);
       // PHILIPS_Gyroscan-12-MONO2-Jpeg_Lossless.dcm
       if(!r)
       {
@@ -186,7 +192,8 @@ bool JPEGCodec::Decode(DataElement const &in, DataElement &out)
         if(i >= this->GetDimensions()[2])
         {
           // JPEGInvalidSecondFrag.dcm
-          assert(nfrags == this->GetNumberOfDimensions()); (void)nfrags; // sentinel
+          assert(nfrags == this->GetNumberOfDimensions());
+          (void)nfrags;
           mdcmAlwaysWarnMacro("JPEG: invalid fragment found at #" << i + 1 << ", skipped");
         }
         else
@@ -200,40 +207,51 @@ bool JPEGCodec::Decode(DataElement const &in, DataElement &out)
   {
     // GEIIS Icon
     std::stringstream is0;
-    size_t jpegbv_len = jpegbv->GetLength();
-    char *mybuffer0 = new char[jpegbv_len];
-    bool b0 = jpegbv->GetBuffer(mybuffer0, jpegbv->GetLength());
-    assert(b0); (void)b0;
-    is0.write(mybuffer0, jpegbv->GetLength());
+    const size_t jpegbv_len = jpegbv->GetLength();
+    char * mybuffer0 = new char[jpegbv_len];
+    const bool b0 = jpegbv->GetBuffer(mybuffer0, jpegbv_len);
+    if (!b0)
+    {
+      delete [] mybuffer0;
+      return false;
+    }
+    is0.write(mybuffer0, jpegbv_len);
     delete[] mybuffer0;
-    bool r = DecodeByStreams(is0, os);
+    const bool r = DecodeByStreams(is0, os);
     if(!r)
     {
       // let's try another time: JPEGDefinedLengthSequenceOfFragments.dcm
       is0.seekg(0);
       SequenceOfFragments sf_bug;
-      try { sf_bug.Read<SwapperNoOp>(is0,true); }
+      try
+      {
+        sf_bug.Read<SwapperNoOp>(is0,true);
+      }
       catch (...)
       {
         mdcmAlwaysWarnMacro("JPEG: unknown exception");
         return false;
       }
-      const SequenceOfFragments *sf = &sf_bug;
+      const SequenceOfFragments * sf = &sf_bug;
       for(unsigned int i = 0; i < sf->GetNumberOfFragments(); ++i)
       {
         std::stringstream is;
-        const Fragment &frag = sf->GetFragment(i);
+        const Fragment & frag = sf->GetFragment(i);
         if(frag.IsEmpty())
         {
           mdcmAlwaysWarnMacro("JPEG: frag.IsEmpty() (2)");
           return false;
         }
-        const ByteValue &bv = dynamic_cast<const ByteValue&>(frag.GetValue());
-        size_t bv_len = bv.GetLength();
-        char *mybuffer = new char[bv_len];
-        bool b = bv.GetBuffer(mybuffer, bv.GetLength());
-        assert(b); (void)b;
-        is.write(mybuffer, bv.GetLength());
+        const ByteValue & bv = dynamic_cast<const ByteValue&>(frag.GetValue());
+        const size_t bv_len = bv.GetLength();
+        char * mybuffer = new char[bv_len];
+        const bool b = bv.GetBuffer(mybuffer, bv_len);
+        if (!b)
+        {
+          delete [] mybuffer;
+          return false;
+        }
+        is.write(mybuffer, bv_len);
         delete[] mybuffer;
         const bool r2 = DecodeByStreams(is, os);
         if(!r2)
@@ -258,10 +276,10 @@ void JPEGCodec::ComputeOffsetTable(bool)
   // Not implemented
 }
 
-bool JPEGCodec::GetHeaderInfo(std::istream & is, TransferSyntax &ts)
+bool JPEGCodec::GetHeaderInfo(std::istream & is, TransferSyntax & ts)
 {
   assert(Internal);
-  if (!Internal->GetHeaderInfo(is, ts))
+  if(!Internal->GetHeaderInfo(is, ts))
   {
     // check if this is one of those buggy lossless JPEG
     if(this->BitSample != Internal->BitSample)
@@ -306,20 +324,20 @@ bool JPEGCodec::Code(DataElement const &in, DataElement &out)
   out = in;
   SmartPointer<SequenceOfFragments> sq = new SequenceOfFragments;
   const Tag itemStart(0xfffe, 0xe000);
-  const ByteValue *bv = in.GetByteValue();
+  const ByteValue * bv = in.GetByteValue();
   if(!bv) return false;
-  const unsigned int *dims = this->GetDimensions();
-  const char *input = bv->GetPointer();
-  unsigned long len = bv->GetLength();
-  unsigned long image_len = len / dims[2];
+  const unsigned int * dims = this->GetDimensions();
+  const char * input = bv->GetPointer();
+  const size_t len = bv->GetLength();
+  const size_t image_len = len / dims[2];
   if(!Internal) return false;
   Internal->SetLossless(this->GetLossless());
   Internal->SetQuality(this->GetQuality());
   for(unsigned int dim = 0; dim < dims[2]; ++dim)
   {
     std::stringstream os;
-    const char *p = input + dim * image_len;
-    bool r = Internal->InternalCode(p, image_len, os);
+    const char * p = input + dim * image_len;
+    const bool r = Internal->InternalCode(p, image_len, os);
     if(!r) return false;
     std::string str = os.str();
     assert(str.size());
@@ -337,10 +355,10 @@ bool JPEGCodec::Code(DataElement const &in, DataElement &out)
 bool JPEGCodec::DecodeByStreams(std::istream &is, std::ostream &os)
 {
   std::stringstream tmpos;
-  if (!Internal->DecodeByStreams(is,tmpos))
+  if(!Internal->DecodeByStreams(is,tmpos))
   {
 #ifdef MDCM_SUPPORT_BROKEN_IMPLEMENTATION
-    // let's check if this is one of those buggy lossless JPEG
+    // check if this is one of the buggy lossless JPEG
     if(this->BitSample != Internal->BitSample)
     {
       // MARCONI_MxTWin-12-MONO2-JpegLossless-ZeroLengthSQ.dcm
@@ -410,7 +428,7 @@ bool JPEGCodec::IsValid(PhotometricInterpretation const &pi)
 }
 
 bool JPEGCodec::DecodeExtent(
-    char *buffer,
+    char * buffer,
     unsigned int xmin, unsigned int xmax,
     unsigned int ymin, unsigned int ymax,
     unsigned int zmin, unsigned int zmax,
@@ -420,7 +438,7 @@ bool JPEGCodec::DecodeExtent(
   bot.Read<SwapperNoOp>(is);
   const unsigned int * dimensions = this->GetDimensions();
   const PixelFormat & pf = this->GetPixelFormat();
-  if (pf == PixelFormat::SINGLEBIT) 
+  if(pf == PixelFormat::SINGLEBIT) 
   {
     mdcmAlwaysWarnMacro("JPEG: pf == PixelFormat::SINGLEBIT");
     return false;
@@ -429,7 +447,6 @@ bool JPEGCodec::DecodeExtent(
   {
     std::vector<char> vdummybuffer;
     size_t buf_size = 0;
-
     const Tag seqDelItem(0xfffe,0xe0dd);
     Fragment frag;
     unsigned int nfrags = 0;
@@ -438,8 +455,8 @@ bool JPEGCodec::DecodeExtent(
       while(frag.ReadPreValue<SwapperNoOp>(is) && frag.GetTag() != seqDelItem)
       {
         ++nfrags;
-        size_t fraglen = frag.GetVL();
-        size_t oldlen = vdummybuffer.size();
+        const size_t fraglen = frag.GetVL();
+        const size_t oldlen = vdummybuffer.size();
         buf_size = fraglen + oldlen;
         vdummybuffer.resize(buf_size);
         is.read(&vdummybuffer[oldlen], fraglen);
@@ -463,11 +480,10 @@ bool JPEGCodec::DecodeExtent(
       else if (frag.GetTag() == Tag(0xddff,0x00e0))
       {
         assert(nfrags == 1);
-        const ByteValue *bv = frag.GetByteValue();
-        assert((unsigned char)bv->GetPointer()[ bv->GetLength() - 1 ] == 0xfe);
+        const ByteValue * bv = frag.GetByteValue();
+        assert((unsigned char)bv->GetPointer()[bv->GetLength() - 1] == 0xfe);
         // Yes this is an extra copy, this is a bug anyway, go fix YOUR code
         frag.SetByteValue(bv->GetPointer(), bv->GetLength() - 1);
-        assert(0);
         mdcmAlwaysWarnMacro("JPEG: fragment length was declared with an extra byte");
         is.clear(); // clear the error bit
       }
@@ -495,10 +511,10 @@ bool JPEGCodec::DecodeExtent(
     const unsigned int bytesPerPixel = pf.GetPixelSize();
     os.seekg(0, std::ios::beg);
     assert(os.good());
-    std::istream *theStream = &os;
+    std::istream * theStream = &os;
     std::vector<char> buffer1;
     buffer1.resize(rowsize*bytesPerPixel);
-    char *tmpBuffer1 = &buffer1[0];
+    char * tmpBuffer1 = &buffer1[0];
     unsigned int y, z;
     std::streamoff theOffset;
     for (z = zmin; z <= zmax; ++z)
@@ -552,7 +568,7 @@ bool JPEGCodec::DecodeExtent(
       unsigned int bytesPerPixel = pf.GetPixelSize();
       std::vector<char> buffer1;
       buffer1.resize(rowsize*bytesPerPixel);
-      char *tmpBuffer1 = &buffer1[0];
+      char * tmpBuffer1 = &buffer1[0];
       unsigned int y;
       std::streamoff theOffset;
       for (y = ymin; y <= ymax; ++y)
@@ -561,8 +577,7 @@ bool JPEGCodec::DecodeExtent(
         theOffset = 0 + (0*dimensions[1]*dimensions[0] + y*dimensions[0] + xmin)*bytesPerPixel;
         theStream->seekg(theOffset);
         theStream->read(tmpBuffer1, rowsize*bytesPerPixel);
-        memcpy(
-          &(buffer[((z-zmin)*rowsize*colsize + (y-ymin)*rowsize)*bytesPerPixel]),
+        memcpy(&(buffer[((z-zmin)*rowsize*colsize + (y-ymin)*rowsize)*bytesPerPixel]),
           tmpBuffer1, rowsize*bytesPerPixel);
       }
     }
@@ -578,8 +593,8 @@ bool JPEGCodec::IsStateSuspension() const
 
 ImageCodec * JPEGCodec::Clone() const
 {
-  JPEGCodec *copy = new JPEGCodec;
-  ImageCodec &ic = *copy;
+  JPEGCodec * copy = new JPEGCodec;
+  ImageCodec & ic = *copy;
   ic = *this;
   assert(copy->PF == PF);
   copy->SetPixelFormat(GetPixelFormat());
@@ -589,7 +604,7 @@ ImageCodec * JPEGCodec::Clone() const
 }
 
 bool JPEGCodec::EncodeBuffer(std::ostream & out,
-    const char *inbuffer, size_t inlen)
+    const char * inbuffer, size_t inlen)
 {
   assert(Internal);
   return Internal->EncodeBuffer(out, inbuffer, inlen);
@@ -599,19 +614,23 @@ bool JPEGCodec::StartEncode(std::ostream &)
 {
   return true;
 }
+
 bool JPEGCodec::IsRowEncoder()
 {
   return true;
 }
+
 bool JPEGCodec::IsFrameEncoder()
 {
   assert(0);
   return false;
 }
+
 bool JPEGCodec::AppendRowEncode(std::ostream & os, const char * data, size_t datalen)
 {
   return EncodeBuffer(os, data, datalen);
 }
+
 // TODO: technically the frame encoder could use the row encoder when present
 // this could reduce code duplication
 bool JPEGCodec::AppendFrameEncode(std::ostream & , const char * , size_t)
@@ -619,6 +638,7 @@ bool JPEGCodec::AppendFrameEncode(std::ostream & , const char * , size_t)
   assert(0);
   return false;
 }
+
 bool JPEGCodec::StopEncode(std::ostream &)
 {
   return true;
