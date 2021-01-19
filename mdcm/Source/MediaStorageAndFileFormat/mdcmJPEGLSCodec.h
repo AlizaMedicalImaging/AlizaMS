0fd2a2f90f2e76b9a0e4933237c124e4bf34ede0
Mihail Isakov <mihail.isakov@gmail.com>
2021-01-19 10:03:30 +0100

    MDCM style

--- mdcm/Source/MediaStorageAndFileFormat/mdcmJPEGLSCodec.h
+++ mdcm/Source/MediaStorageAndFileFormat/mdcmJPEGLSCodec.h
@@ -28,68 +28,48 @@ namespace mdcm
 {
 
 class JPEGLSInternals;
+
 /**
- * \brief JPEG-LS
- * \note codec that implement the JPEG-LS compression
- * this is an implementation of ImageCodec for JPEG-LS
- *
- * It uses the CharLS JPEG-LS implementation https://github.com/team-charls/charls
+ * JPEG-LS
+ * CharLS JPEG-LS implementation https://github.com/team-charls/charls
  */
 class MDCM_EXPORT JPEGLSCodec : public ImageCodec
 {
 friend class ImageRegionReader;
 public:
   JPEGLSCodec();
-  ~JPEGLSCodec() /*override*/;
-  bool CanDecode(TransferSyntax const &) const /*override*/;
-  bool CanCode(TransferSyntax const &) const /*override*/;
-
+  ~JPEGLSCodec();
+  bool CanDecode(TransferSyntax const &) const;
+  bool CanCode(TransferSyntax const &) const;
   unsigned long long GetBufferLength() const { return BufferLength; }
   void SetBufferLength(unsigned long long l) { BufferLength = l; }
-
-  bool Decode(DataElement const & is, DataElement & os) /*override*/;
-  bool Decode(
-    DataElement const & in, char * outBuffer, size_t inBufferLength,
-    uint32_t inXMin, uint32_t inXMax, uint32_t inYMin,
-    uint32_t inYMax, uint32_t inZMin, uint32_t inZMax);
-  bool Code(DataElement const &in, DataElement &out) /*override*/;
-
-  bool GetHeaderInfo(std::istream &, TransferSyntax &) /*override*/;
-  ImageCodec * Clone() const /*override*/;
-
-  void SetLossless(bool l);
+  bool Decode(DataElement const &, DataElement &);
+  bool Decode(DataElement const &, char *, size_t,
+    uint32_t, uint32_t, uint32_t,
+    uint32_t, uint32_t, uint32_t);
+  bool Code(DataElement const &, DataElement &);
+  bool GetHeaderInfo(std::istream &, TransferSyntax &);
+  ImageCodec * Clone() const;
+  void SetLossless(bool);
   bool GetLossless() const;
-
-/*
- * test.acr can look pretty bad, even with a lossy error of 2. Explanation follows:
- * I agree that the test image looks ugly. In this particular case I can
- * explain though.
- *
- * The image is 8 bit, but it does not use the full 8 bit dynamic range. The
- * black pixels have value 234 and the white 255. If you set allowed lossy
- * error to 2, you allow an error of about 10% of the actual dynamic range.
- * That is of course very visible.
- */
-  /// [0-3] generally
-  void SetLossyError(int error);
+  void SetLossyError(int); // [0-3] generally
 
 protected:
-  bool DecodeExtent(
-    char *buffer,
-    unsigned int xmin, unsigned int xmax,
-    unsigned int ymin, unsigned int ymax,
-    unsigned int zmin, unsigned int zmax,
-    std::istream & is);
-  bool StartEncode( std::ostream & ) /*override*/;
-  bool IsRowEncoder() /*override*/;
-  bool IsFrameEncoder() /*override*/;
-  bool AppendRowEncode( std::ostream & out, const char * data, size_t datalen ) /*override*/;
-  bool AppendFrameEncode( std::ostream & out, const char * data, size_t datalen ) /*override*/;
-  bool StopEncode( std::ostream & ) /*override*/;
+  bool DecodeExtent(char *,
+    unsigned int, unsigned int,
+    unsigned int, unsigned int,
+    unsigned int, unsigned int,
+    std::istream &);
+  bool StartEncode(std::ostream &);
+  bool IsRowEncoder();
+  bool IsFrameEncoder();
+  bool AppendRowEncode(std::ostream &, const char *, size_t);
+  bool AppendFrameEncode(std::ostream &, const char *, size_t);
+  bool StopEncode(std::ostream &);
 
 private:
-  bool DecodeByStreamsCommon(const char * buffer, size_t totalLen, std::vector<unsigned char> & rgbyteOut);
-  bool CodeFrameIntoBuffer(char * outdata, size_t outlen, size_t & complen, const char * indata, size_t inlen );
+  bool DecodeByStreamsCommon(const char *, size_t, std::vector<unsigned char> &);
+  bool CodeFrameIntoBuffer(char *, size_t, size_t &, const char *, size_t);
   unsigned long long BufferLength;
   int LossyError;
 };
