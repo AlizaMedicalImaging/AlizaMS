afc0be8257349fc85900eaedc2b4bc110d3d53c3
Mihail Isakov <mihail.isakov@gmail.com>
2024-01-29 13:41:43 +0100

    Removed unnecessary

--- common/mmath.cpp
+++ common/mmath.cpp
@@ -1,13 +1,6 @@
-//#define A_MMATH_DEBUG
-
 #include "mmath.h"
 #include <cmath>
 #include <cstring>
-#ifdef A_MMATH_DEBUG
-#include <iostream>
-#endif
-
-// TODO maybe std::isinf, std::isnan
 
 bool MMath::AlmostEqual(
 	float A, float B,
@@ -17,12 +10,6 @@ bool MMath::AlmostEqual(
 	static_assert(sizeof(float) == 4 && sizeof(int) == 4, "");
 	if (std::fabs(A - B) <= max_diff)
 	{
-#ifdef A_MMATH_DEBUG
-		std::cout
-			<< "(float) A=" << A << ", B=" << B
-			<< ", std::fabs(A - B)=" << std::fabs(A - B)
-			<< ", max_diff=" << max_diff << std::endl;
-#endif
 		return true;
 	}
 	if (std::signbit(A) != std::signbit(B))
@@ -34,12 +21,6 @@ bool MMath::AlmostEqual(
 	std::memcpy(&uA, &A, 4);
 	std::memcpy(&uB, &B, 4);
 	const int ulps_diff = std::abs(uA - uB);
-#ifdef A_MMATH_DEBUG
-	std::cout
-		<< "(float) A=" << A << ", B=" << B
-		<< ", ulps_diff=" << ulps_diff
-		<< ", max_ulps_diff=" << max_ulps_diff << std::endl;
-#endif
 	if (ulps_diff <= max_ulps_diff)
 	{
 		return true;
@@ -55,12 +36,6 @@ bool MMath::AlmostEqual(
 	static_assert(sizeof(double) == 8 && sizeof(long long) == 8, "");
 	if (std::fabs(A - B) <= max_diff)
 	{
-#ifdef A_MMATH_DEBUG
-		std::cout
-			<< "(double) A=" << A << ", B=" << B
-			<< ", std::fabs(A - B)=" << std::fabs(A - B)
-			<< ", max_diff=" << max_diff << std::endl;
-#endif
 		return true;
 	}
 	if (std::signbit(A) != std::signbit(B))
@@ -72,12 +47,6 @@ bool MMath::AlmostEqual(
 	std::memcpy(&uA, &A, 8);
 	std::memcpy(&uB, &B, 8);
 	const long long ulps_diff = std::abs(uA - uB);
-#ifdef A_MMATH_DEBUG
-	std::cout
-		<< "(double) A=" << A << ", B=" << B
-		<< ", ulps_diff=" << ulps_diff
-		<< ", max_ulps_diff=" << max_ulps_diff << std::endl;
-#endif
 	if (ulps_diff <= max_ulps_diff)
 	{
 		return true;
@@ -92,30 +61,16 @@ bool MMath::AlmostEqual(
 	const long double diff = std::fabs(A - B);
 	if (diff <= max_diff)
 	{
-#ifdef A_MMATH_DEBUG
-		std::cout
-			<< "(long double) A=" << A << ", B=" << B
-			<< ", diff=" << diff << ", max_diff=" << max_diff << std::endl;
-#endif
 		return true;
 	}
 	const long double uA = std::fabs(A);
 	const long double uB = std::fabs(B);
 	const long double diff2 =
 		((uB > uA) ? uB : uA) * std::numeric_limits<long double>::epsilon();
-#ifdef A_MMATH_DEBUG
-	std::cout
-		<< "(long double) A=" << A << ", B=" << B
-		<< ", diff=" << diff << ", diff2=" << diff2 << std::endl;
-#endif
 	if (diff <= diff2)
 	{
 		return true;
 	}
 	return false;
 }
-
-#ifdef A_MMATH_DEBUG
-#undef A_MMATH_DEBUG
-#endif
 
